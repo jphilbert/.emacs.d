c1348dba4ccae5bac4bec9c3a72f5299a3e046e5
John P. Hilbert <jphilbert@gmail.com>
2013-07-23 08:47:34 -0400

    Fixed Frame Settings

--- settings/frame-settings.el
+++ settings/frame-settings.el
@@ -1,6 +1,65 @@
-(provide 'frame-settings)
+;; -----------------------------------------------------------------------------
+;; FRAME-SETTINGS.EL --- Simple description
+;; -----------------------------------------------------------------------------
+;; Filename:		FRAME-SETTINGS.EL
+;; Description:		Simple description
+;; Author:		John P. Hilbert <jphilbert@gmail.com>
+;; Maintainer:		John P. Hilbert <jphilbert@gmail.com>
+;; Copyright (C)	2013, John P. Hilbert, all rights reserved.
+;; Created:		2013-07-23 08:42:02
+;; Version:		0.1
+;; Last-Updated:	2013-07-23 08:42:02
+;;           By:	John P. Hilbert
+;; URL:			
+;; Keywords:		<NONE>
+;; Compatibility:	GNU Emacs 23.3.1
+;;
+;; Features that might be required by this library:
+;; <NONE>
 
+;; !!!This file is NOT part of GNU Emacs!!!
 
+;; -----------------------------------------------------------------------------
+;; License
+;; -----------------------------------------------------------------------------
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;; -----------------------------------------------------------------------------
+;; Commentary:
+;; -----------------------------------------------------------------------------
+;; Simple description
+
+;; 
+;;
+
+;; -----------------------------------------------------------------------------
+;; Installation:
+;; -----------------------------------------------------------------------------
+;; Put FRAME-SETTINGS.EL to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;;	(add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;	(require 'frame-settings)
+;;	
+
+;; -----------------------------------------------------------------------------
+;; Customize:
+;; -----------------------------------------------------------------------------
 (setq-default fit-frame-max-width Multi-Window-Default-Window-Width)
 
 (defvar Multi-Window-Terminal-Window-Top
@@ -8,7 +67,37 @@
   "Default terminal window top position")
 (defvar Multi-Window-Terminal-Window-Left
   (+ (x-display-pixel-width) 1)
-    "Default terminal window left position")
+  "Default terminal window left position")
+
+;; All of the above can customize by:
+;;      M-x customize-group RET frame-settings RET
+;;
+
+;; -----------------------------------------------------------------------------
+;; Change log:
+;; -----------------------------------------------------------------------------
+;; 2013-07-23
+;;      * First released.
+;; 
+
+;; -----------------------------------------------------------------------------
+;; Acknowledgments:
+;; -----------------------------------------------------------------------------
+;; 
+
+;; -----------------------------------------------------------------------------
+;; TODO:
+;; -----------------------------------------------------------------------------
+;; 
+
+;; -----------------------------------------------------------------------------
+;; Require
+;; -----------------------------------------------------------------------------
+;; <NONE>
+
+;; -----------------------------------------------------------------------------
+;; Code:
+;; -----------------------------------------------------------------------------
 
 
 ;; Common Display Properties
@@ -151,3 +240,8 @@
 	     `(left . ,(+ 30 Multi-Window-Terminal-Window-Left)))))
 
 
+
+
+(provide 'frame-settings)
+
+;;; FRAME-SETTINGS.EL ends here
