aaaa845120386da1839c8019711a40a82a7f43ad
Hilbert <hilbertjp2@upmc.edu>
2018-02-05 13:45:05 -0500

    updated packages

---
+++ elpa/sql-indent-20170112.1507/sql-indent-pkg.el
@@ -0,0 +1,1 @@
+(define-package "sql-indent" "20170112.1507" "indentation of SQL statements" 'nil :commit "761a5724d181b75f30e64040408b8836d41f9db9" :url "https://github.com/bsvingen/sql-indent" :keywords '("languages"))
