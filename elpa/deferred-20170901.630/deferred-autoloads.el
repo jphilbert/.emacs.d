b1c3e0dfc24b21e075cc48065f76a3a1eacf04df
Hilbert <hilbertjp2@upmc.edu>
2018-02-06 08:32:43 -0500

    package updates

--- elpa/deferred-20170901.630/deferred-autoloads.el
+++ elpa/deferred-20170901.630/deferred-autoloads.el
@@ -3,6 +3,10 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
+;;;### (autoloads nil nil ("deferred.el") (23160 51810 0 0))
+
+;;;***
+
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
--- elpa/web-mode-20180204.251/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20180204.251" "major mode for editing web templates" '((emacs "23.1")) :commit "cf7951c2d149f819a16b28e002432a819bc9f305" :url "http://web-mode.org" :keywords '("languages"))
