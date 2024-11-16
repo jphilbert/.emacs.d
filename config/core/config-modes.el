a4433668d2e9548b3c8cc1a2a04ecf68d3b6ec51
John P. Hilbert <jphilbert@gmail.com>
2024-11-16 07:41:15 -0500

    Update config-modes.el
    turned on JS and Powershell

--- config/core/config-modes.el
+++ config/core/config-modes.el
@@ -12,18 +12,20 @@
 ;;                               ;;    | 
 ;; (require 'config-web)         ;;    ├─ CSS           .css
 ;; (require 'config-css)         ;;    ├─ HTML          .html
-;; (require 'config-js)          ;;    ├─ JS            .js
+(require 'config-js)             ;;    ├─ JS            .js
+;;                               ;;    |                .gs
 ;;                               ;;    |
 ;; (require 'config-xml)         ;;    ├─ XML           .xml
 (require 'config-yaml)           ;;    ├─ YAML          .yaml
-;;                               ;;    ├─ Config        .ini / .cfg
+;;                               ;;    ├─ Config        .ini
+;;                               ;;    |                .cfg
 ;;                               ;;    |
 (require 'config-python)         ;;    ├─ Python        .py     <Python>
 (require 'config-r)              ;;    ├─ R             .r      <R>
 (require 'config-sql)            ;;    ├─ SQL           .sql    <SQL>
 ;;                               ;;    |
 ;; (require 'config-shell)       ;;    ├─ Shell         .bat    <DOS>
-;;                               ;;    └─ PowerShell    .ps     <Powershell>
+(require 'config-powershell)     ;;    └─ PowerShell    .ps     <Powershell>
 
 
 (provide 'config-modes)
